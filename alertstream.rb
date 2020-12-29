3049b02cf3d1dde46df4471f3023f916565ab4e9
mmcbride1 <mrmcbride@smcm.edu>
2020-12-29 12:05:50 -0500

    init commit

--- alertstream.rb
+++ alertstream.rb
@@ -2,43 +2,125 @@
 
 require 'net/smtp'
 
-class WebCam
-   
-  # Store number of files in Video folder #
-   @@filecount
+# Class accesses configuration
+class Config
 
-   # Initializes the Video directory to read
-   def initialize(f)
-      @read = f
-   end
-
-   # Set or reset video file count
-   def setcount(i)
-      @@filecount = i
-   end
-
-   # Configuration for email params, etc.
-   def config()
-      n = '/etc/alert.conf'
-      conf = eval(File.open(n) {|f| f.read})
-      return conf
-   end
-
-   # Determine number of files in Video folder
-   def getfilecount()
-      cmd = `ls -1 #{@read} | wc -l`
-      return cmd.to_i
-   end
-
-   # Send alert whenever new video is generated
-   def email(sub)
-      c = config()
-      mesg = "Subject: #{sub}" + "\n" + c[:mesg]
-      smtp = Net::SMTP.new 'smtp.gmail.com', 587
-      smtp.enable_starttls
-      smtp.start('gmail.com', c[:from], c[:pass], :login)
-      smtp.send_message mesg, c[:from], c[:mail]
-      smtp.finish     
-   end
+  # Get config
+  def config()
+     file = 'alert.conf'
+     conf = eval(File.open(file) {|f| f.read})
+     return conf
+  end
 
 end
+
+# Class handles alert 
+# message construction
+class Email
+
+  def initialize(filename)
+    @filename = filename
+  end
+
+  def getconfig
+    config = Config.new.config()
+    return config
+  end
+
+  def buildmsg 
+    content = File.read(@filename)
+    encoded = [content].pack("m")
+    config  = getconfig()
+
+    # Credentials
+    from = config[:from]
+    user = config[:mail]
+    pass = config[:pass]
+    mesg = config[:mesg]
+
+    # Message marker
+    marker  = "AUNIQUEMARKER"
+
+    # Message body
+    body = <<-EOF 
+    #{mesg}
+    EOF
+
+    # Message headers
+    headers = <<-EOF
+    From: #{from}
+    To: #{user}
+    Subject: Motion detected!
+    MIME-Version: 1.0
+    Content-Type: multipart/mixed; boundry = #{marker}
+    --#{marker}
+    EOF
+
+    # Message action
+    action = <<-EOF
+    Content-type: text/plain
+    Content-Transfer-Encoding:8bit
+
+    #{body}
+    --#{marker}
+    EOF
+
+    # Message attachment
+    attachment = <<-EOF
+    Content-Type: multipart/mixed; name = \"#{@filename}\"
+    Content-Transfer-Encoding:base64
+    Content-Disposition: attachment; filename = "#{@filename}"
+
+    #{encoded}
+    --#{marker}--
+    EOF
+
+    # Message
+    message = headers + action + attachment
+    return message
+  end
+
+  def sendmsg
+    # Build message
+    msg = buildmsg()
+    # Send Message
+    begin
+      Net::SMTP.start('gmail.com', from, pass, :login) do |smtp|
+        smtp.sendmail(msg, from, user)
+      end
+    rescue Exception => ex
+      print "Exception occured: " + ex
+    end
+  end
+
+end
+
+# Class provides methods for
+# alert stream
+class Alert
+
+  def initialize(video_dir)
+    @videos = video_dir
+  end
+
+  def getvideo
+    video_file = nil
+    Dir[@videos + "/*"].each do |file|
+      if file.end_with?(".avi")
+        video_file = file
+      end
+    end
+    return video_file
+  end
+
+  def deltevideo(file)
+    path = @videos + "/#{file}"
+    File.delete(path)
+  end
+
+  def sendalert(file)
+    Email.new(file).sendmsg()
+  end
+
+end
+
