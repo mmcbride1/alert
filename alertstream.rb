6125b7be6cbb1cd01e32ca50ae25f3f61b7e3834
mmcbride1 <mrmcbride@smcm.edu>
2020-12-30 13:35:12 -0500

    fixed issues 4

--- alertstream.rb
+++ alertstream.rb
@@ -32,7 +32,6 @@ class Email
     from = config[:from]
     user = config[:mail]
     pass = config[:pass]
-    mesg = config[:mesg]
 
     # Mailer
     mail = MailFactory.new()
@@ -47,15 +46,11 @@ class Email
     mail.attach(@filename)
 
     # Send
-    begin 
-      smtp = Net::SMTP.new 'smtp.gmail.com', 587
-      smtp.enable_starttls
-      smtp.start('gmail.com', from, pass, :login)
-      smtp.sendmail(mail.to_s, from, user)
-      smtp.finish
-    rescue Exception => ex
-      print "Exception occured: " + ex.to_s + "\n"
-    end
+    smtp = Net::SMTP.new 'smtp.gmail.com', 587
+    smtp.enable_starttls
+    smtp.start('gmail.com', from, pass, :login)
+    smtp.sendmail(mail.to_s, from, user)
+    smtp.finish
   end
 
 end
@@ -64,13 +59,13 @@ end
 # alert stream
 class Alert
 
-  def initialize(video_dir)
-    @videos = video_dir
-  end
+  # Video folder
+  @@config = Config.new.config()
+  @@videos = @@config[:vids]
 
   def getvideo
     video_file = nil
-    Dir[@videos + "/*"].each do |file|
+    Dir[@@videos + "/*"].each do |file|
       if file.end_with?(".avi")
         video_file = file
       end
